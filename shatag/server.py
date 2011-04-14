34ef7d950099697ef63d6e2bc301d99d790a634e
Maxime Augier <max@xolus.net>
2011-04-14 11:26:52 +0200

    more debugging

--- shatag/server.py
+++ shatag/server.py
@@ -1,12 +1,15 @@
 from shatag import *
-from flask import Flask
-from flask import request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/host/<hostname>', methods=['POST'])
 def add(host):
     print("host post {0} with data {1}\n".format(host, request.json))
+    return jsonify(dummy=response, hello=world)
 
 def run():
+    app.run()
+
+if __name__ == "__main__":
     app.run()
