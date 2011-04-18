398f393625c7b05ad9b000745a0a9348e7189f9d
Maxime Augier <max@xolus.net>
2011-04-18 20:28:19 +0200

    CouchDB prototype backend, not working

--- shatag.py
+++ shatag.py
@@ -117,8 +117,9 @@ def Store(url=None, name=None):
         if url is None:
             url = '{0}/.shatagdb'.format(os.environ['HOME'])
     
-        if url.startswith("http://"):
-            return HTTPStore(url, name)
+        if url.startswith("http://") or url.startswith("https://"):
+	    from shatag.couchdb import CouchStore
+            return CouchStore(url, name)
         else:
             return LocalStore(url, name)
 
---
+++ shatag/couchdb.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# Copyright 2010 Maxime Augier
+# Distributed under the terms of the GNU General Public License
+
+#import argparse
+from shatag import *
+import json
+import couchdb
+
+
+class CouchStore(IStore):
+
+    def __init__(self, url=None, name=None):
+        super().__init__(url, name)
+
+	server = couchdb.Server(url)
+	self.db = server['shatag']
+
+
+    def clear(self, base='/'):
+    	pass
+
+    def put(self, file):
+        self.record(self.name, file.fullpath(), file.shatag)
+
+    def record(self, name, path, tag):
+        self.db.save({'hash':tag, 'name':name, 'path':path})
+
+    def lookup(self, file, remotenames=None):
+
+        local = list()
+        remote = list()
+
+        if file.state != 'good':
+            raise NoChecksum()
+
+        for (name, path) in self.fetch():
+        
+            if ((remotenames is None and name != self.name) or
+               (remotenames is not None and name in remotenames)):
+                    remote.append((name,path))
+            elif path != file.fullpath():
+                local.append((name,path))
+
+        return StoreResult(file, remote, local) 
+
+    def fetch(self, hash):
+    	pass	
+
+    def commit(self):
+        self.db.commit()
+
+    def rollback(self):
+        self.db.rollback()
+
