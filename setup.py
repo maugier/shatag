526c06bf3131e2451a82f2e4dda2a483619ce49c
Maxime Augier <max@xolus.net>
2011-04-19 19:21:01 +0200

    bump distutils version to 0.2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='shatag',
-      version = '0.1',
+      version = '0.2',
       description = 'File checksumming utility',
       author = 'Maxime Augier',
       author_email = 'max@xolus.net',
