3c4ee13798a39734bc52cff277ce931b5222c6e7
Maxime Augier <max@xolus.net>
2011-04-19 13:26:38 +0200

    Special parametrized query handling for psycopg2

--- shatag/pg.py
+++ shatag/pg.py
@@ -20,5 +20,20 @@ class PgStore(SQLStore):
             cursor.execute('create table contents(hash varchar(64), name varchar(50), path varchar(100), primary key (name, path))')
             cursor.execute('create index content_hash on contents(hash)')
         except psycopg2.ProgrammingError as e:
-            pass
+            db.rollback()
+
+    # reimplementing these because psycopg2 does not handle classic placeholders correctly
+    def clear(self, base='/'):
+        self.cursor.execute('delete from contents where name = %(name)s and substr(path,1,length(%(base)s)) like %(base)s', {'name': self.name, 'base': base})
+        return self.cursor.rowcount
+
+
+    def record(self, name, path, tag):
+        d = {'name': name, 'path': path, 'tag':tag }
+        self.cursor.execute('delete from contents where name = %(name)s and path = %(path)s', d)
+        self.cursor.execute('insert into contents(hash,name,path) values(%(tag)s,%(name)s,%(path)s)', d)
+
+    def fetch(self,hash):
+        self.cursor.execute('select name,path from contents where hash = %(hash)s', {'hash':hash})
+        return self.cursor 
 
