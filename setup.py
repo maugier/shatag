5e0a82e568dbe9af81d60841fc313fdbf4d588f3
Maxime Augier <max@xolus.net>
2012-03-12 22:32:46 +0100

    Fix missing stores package

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='shatag',
       author = 'Maxime Augier',
       author_email = 'max@xolus.net',
       url = 'http://bitbucket.org/maugier/shatag',
-      packages = ['shatag','shatag.backend'],
+      packages = ['shatag','shatag.backend','shatag.store'],
       install_requires=['argparse'],
       scripts = ['bin/shatag', 'bin/shatag-add', 'bin/shatag-mkdb', 'bin/shatagd'])
