69330abc7ff239ace12517521a8aa501b66563a3
Maxime Augier <max@xolus.net>
2012-03-12 23:54:29 +0100

    HTTP store based on requests, read-only

--- shatag/store/http.py
+++ shatag/store/http.py
@@ -2,13 +2,21 @@
 # Copyright 2011 Maxime Augier
 # Distributed under the terms of the GNU General Public License
 
-import http.client
+import requests
+import json
 import shatag.base
 
 class Store(shatag.base.IStore):
     def __init__(self,url):
         super().__init__(url, name)
+        self.session = requests.session()
 
+    def call(self,url):
+        return json.loads(self.session.get(url, prefetch=True).text)
 
     def fetch(self,hash):
-    	pass
+    	data = self.call(self.url + '/find/' + hash)
+	for item in data[hash]:
+            yield (item['host'], item['file'])
+
+	
