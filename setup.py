80420584073d762d7f521de7de5bbc63aac70b7a
Maxime Augier <max@xolus.net>
2012-03-16 11:03:05 +0100

    Remove dependancy to argparse as it is part of python 3.2

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='shatag',
       author_email = 'max@xolus.net',
       url = 'http://bitbucket.org/maugier/shatag',
       packages = ['shatag','shatag.backend','shatag.store'],
-      install_requires=['argparse'],
+      #install_requires=['argparse'],
       scripts = ['bin/shatag', 'bin/shatag-add', 'bin/shatag-mkdb', 'bin/shatagd'])
