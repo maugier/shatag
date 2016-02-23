8b15285c9c9c9ba9e47e48a5103869b03a5564c1
Maxime Augier <max@xolus.net>
2016-02-23 17:24:35 +0100

    Now that xattr supports bytestrings for filenames, use proper encoding with surrogate support

--- shatag/backend/xattr.py
+++ shatag/backend/xattr.py
@@ -1,3 +1,4 @@
+from os import fsencode
 import shatag.base
 import xattr
 
@@ -6,15 +7,15 @@ class File(shatag.base.IFile):
     def read(self):
 
         try:
-            self.ts = xattr.getxattr(self.filename, 'user.shatag.ts').decode('ascii')
-            self.shatag = xattr.getxattr(self.filename, 'user.shatag.sha256').decode('ascii')
+            self.ts = xattr.getxattr(fsencode(self.filename), 'user.shatag.ts').decode('ascii')
+            self.shatag = xattr.getxattr(fsencode(self.filename), 'user.shatag.sha256').decode('ascii')
         except IOError as e:
             if e.errno != 61:  # no tag present
                raise e
 
     def write(self):
-        xattr.setxattr(self.filename, 'user.shatag.sha256', self.shatag.encode('ascii'))
-        xattr.setxattr(self.filename, 'user.shatag.ts', self.mtime.encode('ascii'))
+        xattr.setxattr(fsencode(self.filename), 'user.shatag.sha256', self.shatag.encode('ascii'))
+        xattr.setxattr(fsencode(self.filename), 'user.shatag.ts', self.mtime.encode('ascii'))
 
 
 class Backend:
